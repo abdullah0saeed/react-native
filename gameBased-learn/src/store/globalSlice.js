63c1be9ba1a263aa064436fc26ba3b55f27b40d8
Abdullah Saeed <abdullahhamouda056@gmail.com>
2023-02-18 02:28:58 +0200

    full connection with back

--- gameBased-learn/src/store/globalSlice.js
+++ gameBased-learn/src/store/globalSlice.js
@@ -6,12 +6,23 @@ export const fetchData = createAsyncThunk(
   async (_, thunkAPI) => {
     const { rejectedWithValue, getState } = thunkAPI;
     const parentID = getState().auth.parentID;
+    const sentData = JSON.stringify({
+      unit: "1",
+      lesson: "1",
+      stadge: "1",
+    });
     try {
       const res = await fetch(
-        `http://192.168.1.2:3000/FSE/FSEtakeQuestion/${parentID}`
+        `http://192.168.1.2:3000/FSE/FSEtakeQuestion/${parentID}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: sentData,
+        }
       );
       const data = await res.json();
-      console.log(data);
 
       return data;
     } catch (error) {
@@ -37,7 +48,8 @@ const globalSlice = createSlice({
     },
     [fetchData.fulfilled]: (state, action) => {
       state.loading = false;
-      state.word_Pic = action.payload;
+      state.word_Pic = action.payload.question;
+      console.log(state.word_Pic);
       console.log("fetch success");
     },
     [fetchData.rejected]: (state, action) => {
